de2f03a1cae0644044695cb5c914a024f2d8a012
TurkiSultan <109402335+TurkiSultan@users.noreply.github.com>
2022-07-28 07:25:26 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var change = document.getElementById("body");
+var p1 = document.getElementById("p1");
+var p2 = document.getElementById("p2");
+var p3 = document.getElementById("p3");
+var p4 = document.getElementById("p4");
+var p5 = document.getElementById("p5");
+var p6 = document.getElementById("p6");
+var f_controaler = document.getElementById("f_controaler");
+var f_controaler2 = document.getElementById("f_controaler2");
+var footer_end = document.getElementById("footer_end");
+var footer_end = document.getElementById("fixd-right");
+/* buttom */
+
+
+function night() {
+    change.classList.toggle("black");
+    p1.classList.toggle("all_p");
+    p2.classList.toggle("all_p");
+    p3.classList.toggle("all_p");
+    p4.classList.toggle("all_p");
+    p5.classList.toggle("all_p");
+    p6.classList.toggle("all_p");
+    f_controaler.classList.toggle("form_controaler");
+    f_controaler2.classList.toggle("form_controaler");
+    footer_end.classList.toggle("orang");
+  
+}
